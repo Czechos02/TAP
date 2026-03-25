bb6379fc6487f28262c4497e7c77d0cd590e1d69
Your Name <you@example.com>
2026-03-25 12:55:26 +0100

    sprawdzony kod, wszystko smiga

--- model_przestrzen_stanu.m
+++ model_przestrzen_stanu.m
@@ -22,10 +22,8 @@ p.V = 1;
 % WARUNKI POCZĄTKOWE
 % ==============================================
 
-y0   = [0.26 393.9];
-u0   = [2 15 323 365];   
-z0 = [323 365];
-sim_time = [0, 20];      
+y0   = [0.26 393.9]; % zmienne stanu (Ca, T) w pp
+u0   = [2 15 323 365]; % sterowania (Cain, Fc) i zaklocenia (Tin, Tcin) w pp  
 
 %% Model LTI %% ciagly DO SPRAWDZENIA
 
@@ -45,7 +43,7 @@ du(:,4) = (t >= step_time) * 1;     % skok Tcin
 sys_c = ss(A,B,C,D);
 
 %% Model LTI %% dyskretny DO SPRAWDZENIA
-Ts = 1;
+Ts = 0.1;
 sys_d = c2d(sys_c, Ts, 'zoh');
 t_dysk = (0:Ts:20)';
 
@@ -59,29 +57,46 @@ du_dysk(t_dysk>=step_time,4) = 1;
 % symulacja (BEZ PĘTLI)
 [y_d, t_d, x_d] = lsim(sys_d, du_dysk, t_dysk);
 
-% powrót do wartości absolutnych
 y1_d = y_d(:,1) + y0(1);
 y2_d = y_d(:,2) + y0(2);
 
-
+% Transmitancja dyskretna
 Gz = tf(sys_d)
 
 figure;
-subplot(2,1,1);
+subplot(3,2,1);
 stairs(t_d, y1_d, 'b','LineWidth',1.5);
 title('C_A (dyskretny)');
 grid on;
 
-subplot(2,1,2);
+subplot(3,2,2);
 stairs(t_d, y2_d, 'c','LineWidth',1.5);
 title('T (dyskretny)');
 grid on;
 
+subplot(3,2,3);
+plot(t_d, du_dysk(:,1), 'y', 'LineWidth', 1.5);
+xlabel('T_dysk'); ylabel('u_1'); title('u_1');
+grid on;
 
-%% DO SPRAWDZENIA %%
-% symulacja ciagla LTI
+subplot(3,2,4);
+plot(t_d, du_dysk(:,2), 'y', 'LineWidth', 1.5);
+xlabel('T_dysk'); ylabel('u_2'); title('u_2');
+grid on;
+
+subplot(3,2,5);
+plot(t_d, du_dysk(:,3), 'g', 'LineWidth', 1.5);
+xlabel('T_dysk'); ylabel('z_1'); title('z_1');
+grid on;
+
+subplot(3,2,6);
+plot(t_d, du_dysk(:,4), 'g', 'LineWidth', 1.5);
+xlabel('T_dysk'); ylabel('z_2'); title('z_2');
+grid on;
+
+%% symulacja ciagla LTI %%
+
 [y_lin, t, x_lin] = lsim(sys_c, du, t);
-% powrót do wartości absolutnych
 y1_lin = y_lin(:,1) + y0(1);
 y2_lin = y_lin(:,2) + y0(2);
 
@@ -114,12 +129,12 @@ xlabel('t'); ylabel('F_c'); title('u_2');
 grid on;
 
 subplot(3,2,5);
-plot(t, Z1, 'k', 'LineWidth', 1.5);
+plot(t, Z1, 'g', 'LineWidth', 1.5);
 xlabel('t'); ylabel('T_{in}'); title('z_1');
 grid on;
 
 subplot(3,2,6);
-plot(t, Z2, 'k', 'LineWidth', 1.5);
+plot(t, Z2, 'g', 'LineWidth', 1.5);
 xlabel('t'); ylabel('T_{cin}'); title('z_2');
 grid on;
 
@@ -127,7 +142,7 @@ sgtitle('Symulacja modelu liniowego (LTI)');
 
 
 % ==============================================
-% JACOBIAN
+% JACOBIAN Macierzy A oraz B
 % ==============================================
 function A = jacobian_A(x,u,p)
 
@@ -168,7 +183,6 @@ exp_term = exp(-p.E_R/x2);
 K = (p.a*u2^(p.b+1)) / (u2 + (p.a*u2^p.b)/(2*p.q*p.c_pc));
 
 % Pochodna dK/du2
-
 N  = p.a * u2^(p.b+1);
 D  = u2 + (p.a*u2^p.b)/(2*p.q*p.c_pc);
 
@@ -176,18 +190,18 @@ dN = p.a * (p.b+1) * u2^p.b;
 dD = 1 + (p.a*p.b*u2^(p.b-1))/(2*p.q*p.c_pc);
 dK = (dN*D - N*dD) / (D^2);
 
-% Macierz B (2x4: [u1 u2 z1 z2])
+% Macierz B (2x4: [u1 u2 u3 u4]) (u3 = z1, u4 = z2)
 B = zeros(2,4);
 
-% ====== f1 ======
+% Pochodne z pierwszego rownania
 B(1,1) = p.F_in / p.V;   % df1/du1
 B(1,2) = 0;              % df1/du2
 B(1,3) = 0;              % df1/dz1
 B(1,4) = 0;              % df1/dz2
 
-% ====== f2 ======
+% Pochodne z drugiego rownania
 B(2,1) = 0; % df2/du1
 B(2,2) = -(x2 - z2)/(p.V*p.q*p.c_p) * dK; % df2/du2
-B(2,3) = p.F_in / p.V; % df2/dz1
-B(2,4) = K / (p.V*p.q*p.c_p); % df2/dz2
+B(2,3) = p.F_in / p.V; % df2/du3
+B(2,4) = K / (p.V*p.q*p.c_p); % df2/du4
 end
